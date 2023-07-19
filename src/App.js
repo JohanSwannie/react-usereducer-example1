bd7d82d15d27bcff540e535ecf9db6daff13d06a
jswan <jswannieman@gmail.com>
2023-07-19 16:55:35 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
 
 function App() {
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case "add":
+        return state + 1;
+      case "subtract":
+        return state - 1;
+      default:
+        return state;
+    }
+  }, 0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      The Count is now : {count}
+      <br />
+      <br />
+      <button onClick={() => dispatch("add")}>Increment</button>
+      <br />
+      <br />
+      <button onClick={() => dispatch("subtract")}>Decrement</button>
     </div>
   );
 }
